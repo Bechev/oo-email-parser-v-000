e2caed8e566ee19204c315f04b44659f76765149
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-10 16:21:58 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,9 +7,9 @@ class EmailParser
     email_list.parse
   end
 
-  def parse(email_list)
-    email_list_wiht_no_space = email_list.split(" ")
-    parsed_email_list = email_list_wiht_no_space.split(",")
+  def self.parse(email_list)
+    email_list_with_no_space = email_list.split(" ")
+    parsed_email_list = email_list_with_no_space.split(",")
     parsed_email_list
   end
 
